d802d7ebb11df71198c73e7700589de93d271154
Raphael Duarte Sena <raphael_duarte@outlook.com>
2020-08-19 16:16:40 -0400

    creating the entities

---
+++ Web/QuickBuy/QuickBuy.Domain/Entities/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace QuickBuy.Domain.Entities
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
---
+++ Web/QuickBuy/QuickBuy.Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace QuickBuy.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// A user can have one or many orders
+        /// </summary>
+        public ICollection<Order> Orders { get; set; }
+
+    }
+}
