f43cb1c65af545e1f39e750c04d5108695fe070b
Raphael Duarte Sena <raphael_duarte@outlook.com>
2021-04-23 17:11:39 -0400

    fixing pagination

--- DatingApp/API/Helpers/PaginationHeader.cs
+++ DatingApp/API/Helpers/PaginationHeader.cs
@@ -2,16 +2,16 @@ namespace API.Helpers
 {
     public class PaginationHeader
     {
-        public PaginationHeader(int currentPage, int itemsPage, int totalItems, int totalPages)
+        public PaginationHeader(int currentPage, int itemsPerPage, int totalItems, int totalPages)
         {
             CurrentPage = currentPage;
-            ItemsPage = itemsPage;
+            ItemsPerPage = itemsPerPage;
             TotalItems = totalItems;
             TotalPages = totalPages;
         }
 
         public int CurrentPage { get; set; }
-        public int ItemsPage { get; set; }
+        public int ItemsPerPage { get; set; }
         public int TotalItems { get; set; }
         public int TotalPages { get; set; }
     }
